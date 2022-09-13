d1f781e5896b50d1c5879769fdef171a3c74c275
Sihab-10 <sihabsarar10@gmail.com>
2022-09-13 15:29:17 +0600

    concatination added

---
+++ concatinatination.js
@@ -0,0 +1,22 @@
+// var price1 = "31";
+// var price2 = "10";
+// var totalPrice = price1 + price2;
+// console.log(totalPrice);
+var price1 = 31;
+var price2 = "10";
+// var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+// convert string to number
+
+console.log(price1);
+console.log(price2);
+var price2Number = parseInt(price2);
+var totalPriceNumber = price1 + price2Number;
+console.log(totalPriceNumber);
+
+/////////////////////////
+var gpa = '3.41';
+var gpaNumber = parseInt(gpa);
+console.log(gpaNumber);
+
