4d3f8012733244ac8fba1d0ee027ad8f1318e909
Sihab-10 <sihabsarar10@gmail.com>
2022-09-13 12:16:10 +0600

    js conversion added

---
+++ conversion.js
@@ -0,0 +1,8 @@
+var price1 = 35;
+var price2 = 10;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+var first_name = 'jennie';
+var last_name = 'kim';
+var fullName = first_name + " " + last_name;
+console.log(fullName);
