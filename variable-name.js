e3a919478974e8b1be36faee7ab864bbbf476c67
Sihab-10 <sihabsarar10@gmail.com>
2022-09-13 09:49:06 +0600

    variabel name added

---
+++ variable-name.js
@@ -0,0 +1,65 @@
+// 16-5 JavaScript Keyword, Variable name naming convention
+
+//   Variable naming convention
+  
+//   1. varibale name can not be any keywords
+  
+var flase = 96;
+var return = true;
+  
+  
+//  2. varibale has to be in one word and no space
+  
+var my home address = "New california";
+
+//   3. variable name cannot have any quotation
+
+var "name" = "sihab";
+
+//  4. variable name can to be start with a number but can end with number 
+
+var 2num = 10;
+
+var num2 = 14; 
+
+
+// 5. how to use long name
+//  can not use dash
+var user-name = "raj bappa";
+// but can use underscore
+var user_name = "sifat";
+
+var user_current_home_address = "andor killa bandor ban";
+var user_home_address = "andor killa bandor ban";//snake case
+var userHomeAddress = "andor killa bandor ban"; //camel case : we will use this one
+var UserHomeAddress = "andor killa bandor ban";//pascal case
+
+// variabel names are case sensitive 
+var name = "joe";
+var Name = "John";
+var namE = "suha";
+// keywords always written as small letter
+var name = "sifat";
+Var name = "sihab";//it is not a correct way
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
