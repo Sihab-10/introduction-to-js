4d2d522b6d2deeb16188e6dabab17760dbaec0ab
Sihab-10 <sihabsarar10@gmail.com>
2022-09-13 11:39:49 +0600

    operation shorthand added

---
+++ shorthand.js
@@ -0,0 +1,39 @@
+var tomatoPrice = 25;//variable declaration
+tomatoPrice = 30;//variable update
+// console.log(tomatoPrice);//print the value
+
+// add something to the previous value
+
+var price1 = 31;
+var price2 = 10; 
+
+// set a new value
+price1 = 35;
+
+// add some value to the existing value
+
+price1 = price1 + 10;
+// console.log(price1);
+
+
+// additional to the same variable shorthand
+// += means adding some value to the existing value
+price1 += 10;
+
+
+price2 = price2 - 5;
+// -= means deducting some values from the current value of the variable 
+price2 -= 5;
+// *= means multiplicate some values from the current value of the variable
+price1 *= 5;
+
+// /= means divide some values from the current value of the variable
+price1 /= 5;
+
+
+var age = 1;
+age++;//means adding 1 to the existing value
+console.log(age);
+var love = 10;
+love--;//means deducting 1 from the existing value
+console.log(love);
