7414bd2dbb9443e89261cac95f68b8d39db01949
Sihab-10 <sihabsarar10@gmail.com>
2022-09-13 08:48:37 +0600

    variable added

---
+++ variable.js
@@ -0,0 +1,14 @@
+// vary + able = variable
+// 5 things you need to declare a variable
+// A variable is a value that can change , depending on conditions or on information passed to the program
+// 1. var keyword is used to declare a variable
+// 2. variable name; variable must be identified with unique names
+// 3. assignment operator is used to assign a value in the variable
+// 4. then assign any value
+// 5. then add ; in the value
+// tips: a variable declared without a value will have the value undefined
+var price = 13;
+var age = 24;
+var temparature = 25;
+var money = 100;
+var weight = 75;
